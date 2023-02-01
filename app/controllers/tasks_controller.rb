1ba9f5a9396677427ad7f42448cdec3ddf4b3eb2
Katsushi OUGI <ougi@to-r.net>
2023-02-01 09:52:01 +0900

    モデルのバリデーション

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,12 +12,32 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new task_params
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    # 検証エラーがあってもう一度新規登録画面を表示する際に、ビューに検証を行った現物の
+    # Taskオブジェクトを渡す必要があるので
+    @task = Task.new(task_params)
+    # ユーザーの入力次第では検証エラーによって登録が失敗するのでsave!ではなくsave
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      # binding.irb
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @task = Task.find params[:id]
+  end
+
+  def update
+    task = Task.find params[:id]
+    task.update task_params
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
+
+  def destroy
+    task = Task.find params[:id]
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   private
